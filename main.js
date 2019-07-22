db900ec2dc55cfe9c0fb9fcca09c16656809eee8
[Yann Mulonda] <[mulo7954@stthomas.edu]>
2019-07-22 16:35:31 -0500

    set up page layout

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ $(document).ready(
 
         var groupVisit_SalesForce_API = 'https://stthomas.force.com/applicantportal/services/apexrest/usteventfeed?eventId=a3s0a000000Auzz&feedType=eventList&viewStart=2019-6-3+00:00:00&viewEnd=2020-5-29+24:59:59';
 
+        var counselor_SalesForce_API;
+
         $.ajax({
 
             type: "GET",
