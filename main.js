aa810df4167440a9cb76da832b00694e388fca28
[Yann Mulonda] <[mulo7954@stthomas.edu]>
2019-07-23 11:12:03 -0500

    updated display layout on select

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Do some stuff when page hmtl page is launched
-$(document).ready(
+$(document).ready( function() {
 
     function showAvaialableVisitDays() {
 
@@ -23,7 +23,7 @@ $(document).ready(
             success: function (data) {
 
                 console.log('data: ', data);
-                
+
 
             } // end:  Ajax success API call
 
@@ -31,4 +31,46 @@ $(document).ready(
 
     } // end: showEvent function
 
-); // end: document.ready()
+}); // end: document.ready()
+
+$(document).on('change', '#userType', function () {
+    console.log($(this).val()); // the selected options’s value
+
+    //it gets you the value of selected option 
+    var value = $(this).val();
+
+    if (value == 'High School Student') {
+
+         $('#transferFrom').hide();
+        $('#ShowSelectState').show();
+
+
+    } else if (value == 'Transfer Student') {
+
+        $('#ShowSelectState').hide();
+        $('#transferFrom').show();
+
+    } else if (value == 'US Veteran') {
+
+        $('#ShowSelectState').hide();
+        $('#transferFrom').hide();
+        console.log('Selected value: ', value);
+
+    } else if (value == 'International Student') {
+
+        console.log('Selected value: ', value);
+        $('#ShowSelectState').hide();
+        $('#transferFrom').hide();
+
+    } else {
+
+        console.log('Selected value: ', value);
+        $('#ShowSelectState').hide();
+        $('#transferFrom').hide();
+    }
+
+    // if you want to do stuff based on the OPTION element:
+    var opt = $(this).find('option:selected')[0];
+    // use switch or if/else etc.
+
+});
